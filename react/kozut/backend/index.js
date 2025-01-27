4a761b453f965a15ee1138989ab4c140fe47fd9d
NagMar206 <nagmar206@hengersor.hu>
2025-01-27 12:15:17 +0100

    25-01-27

--- react/kozut/backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(cors());
-
