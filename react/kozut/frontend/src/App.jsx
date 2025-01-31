c0eec5854953aa800f0d46e71aef6dc33a2f5154
NagMar206 <nagmar206@hengersor.hu>
2025-01-31 14:19:16 +0100

    25-01-31
    Frontend

---
+++ react/kozut/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Közutak</h1>
+    </>
+  )
+}
+
+export default App
